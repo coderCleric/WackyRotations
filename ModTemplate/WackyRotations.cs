6ae28bbca3d5aabe9cc8f0963738741eb3bf0eab
coderCleric <wescamphouse@gmail.com>
2022-09-02 16:46:38 -0600

    README stuff

--- ModTemplate/WackyRotations.cs
+++ ModTemplate/WackyRotations.cs
@@ -53,44 +53,44 @@ namespace WackyRotations
         }
 
         /**
-         * Applies the effects of the config file
+         * Applies the effects of the config file. Max speeds are multiplied by two to convert from rotations/sec to radians/sec
          */
         public override void Configure(IModConfig config)
         {
             //Ash twin
-            ConfigData.UpdateMaxSpeed(PlanetNames.ASH, config.GetSettingsValue<float>("ashMaxSpeed"));
+            ConfigData.UpdateMaxSpeed(PlanetNames.ASH, 2 * config.GetSettingsValue<float>("ashMaxSpeed"));
             ConfigData.UpdateTimeToChange(PlanetNames.ASH, config.GetSettingsValue<float>("ashTTC"));
 
             //Ember twin
-            ConfigData.UpdateMaxSpeed(PlanetNames.EMBER, config.GetSettingsValue<float>("emberMaxSpeed"));
+            ConfigData.UpdateMaxSpeed(PlanetNames.EMBER, 2 * config.GetSettingsValue<float>("emberMaxSpeed"));
             ConfigData.UpdateTimeToChange(PlanetNames.EMBER, config.GetSettingsValue<float>("emberTTC"));
 
             //Timber Hearth
-            ConfigData.UpdateMaxSpeed(PlanetNames.TIMBER, config.GetSettingsValue<float>("timberMaxSpeed"));
+            ConfigData.UpdateMaxSpeed(PlanetNames.TIMBER, 2 * config.GetSettingsValue<float>("timberMaxSpeed"));
             ConfigData.UpdateTimeToChange(PlanetNames.TIMBER, config.GetSettingsValue<float>("timberTTC"));
 
             //Attelrock
-            ConfigData.UpdateMaxSpeed(PlanetNames.ATTELROCK, config.GetSettingsValue<float>("attlMaxSpeed"));
+            ConfigData.UpdateMaxSpeed(PlanetNames.ATTELROCK, 2 * config.GetSettingsValue<float>("attlMaxSpeed"));
             ConfigData.UpdateTimeToChange(PlanetNames.ATTELROCK, config.GetSettingsValue<float>("attlTTC"));
 
             //Brittle Hollow
-            ConfigData.UpdateMaxSpeed(PlanetNames.BRITTLE, config.GetSettingsValue<float>("brittleMaxSpeed"));
+            ConfigData.UpdateMaxSpeed(PlanetNames.BRITTLE, 2 * config.GetSettingsValue<float>("brittleMaxSpeed"));
             ConfigData.UpdateTimeToChange(PlanetNames.BRITTLE, config.GetSettingsValue<float>("brittleTTC"));
 
             //Hollow's Lantern
-            ConfigData.UpdateMaxSpeed(PlanetNames.LANTERN, config.GetSettingsValue<float>("lanternMaxSpeed"));
+            ConfigData.UpdateMaxSpeed(PlanetNames.LANTERN, 2 * config.GetSettingsValue<float>("lanternMaxSpeed"));
             ConfigData.UpdateTimeToChange(PlanetNames.LANTERN, config.GetSettingsValue<float>("lanternTTC"));
 
             //Giant's Deep
-            ConfigData.UpdateMaxSpeed(PlanetNames.DEEP, config.GetSettingsValue<float>("deepMaxSpeed"));
+            ConfigData.UpdateMaxSpeed(PlanetNames.DEEP, 2 * config.GetSettingsValue<float>("deepMaxSpeed"));
             ConfigData.UpdateTimeToChange(PlanetNames.DEEP, config.GetSettingsValue<float>("deepTTC"));
 
             //Dark Bramble
-            ConfigData.UpdateMaxSpeed(PlanetNames.BRAMBLE, config.GetSettingsValue<float>("brambleMaxSpeed"));
+            ConfigData.UpdateMaxSpeed(PlanetNames.BRAMBLE, 2 * config.GetSettingsValue<float>("brambleMaxSpeed"));
             ConfigData.UpdateTimeToChange(PlanetNames.BRAMBLE, config.GetSettingsValue<float>("brambleTTC"));
 
             //Interloper
-            ConfigData.UpdateMaxSpeed(PlanetNames.INTERLOPER, config.GetSettingsValue<float>("interloperMaxSpeed"));
+            ConfigData.UpdateMaxSpeed(PlanetNames.INTERLOPER, 2 * config.GetSettingsValue<float>("interloperMaxSpeed"));
             ConfigData.UpdateTimeToChange(PlanetNames.INTERLOPER, config.GetSettingsValue<float>("interloperTTC"));
         }
 
